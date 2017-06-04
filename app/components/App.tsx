f6dfc37e56d095ea8943d3451f3b6f0d0da80ff6
Fabrice Million <dr.funkensztajn@gmail.com>
2017-06-04 21:47:04 +1000

    Added app scaffolding

---
+++ app/components/App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+
+class App extends React.Component<any, any>{
+  render() {
+    return (
+      <h2 id="heading">Hello world</h2>
+    );
+  }
+}
+
+export = App;
